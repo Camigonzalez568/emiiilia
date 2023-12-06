0df639700fa70669cf059d0152e65d10b8da4c05
camigonzalez568 <cg3857722@gmail.com>
2023-12-06 18:31:29 -0300

    HOLACHAU

---
+++ emilia/compon/Hom.jsx
@@ -0,0 +1,17 @@
+import Cards from './Cards'
+import { useContext } from "react"
+import { ContextProvider } from "../context/Provider";
+import Modal from './Modal';
+function Home() {
+    const {modalOn, setModalOn} = useContext(ContextProvider);
+  return (
+    <div>
+
+       <button onClick={()=>setModalOn(true)}>Agregar Cards</button>
+        {!modalOn&&<Cards/>}
+        {modalOn &&<Modal/>}
+    </div>
+  )
+}
+
+export default Home
---
+++ emilia/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './componentes/Home'
+import MyProvider  from "./context/Provider"
+
+const App = () => {
+  return (
+    < >
+      <MyProvider>
+         
+          <Home/>
+      </MyProvider>
+    </>
+  )
+}
+
+export default A
